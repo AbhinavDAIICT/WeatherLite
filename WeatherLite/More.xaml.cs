0eea88e88f9247863ecb4f4d7df0414e1e79572f
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2014-09-25 19:59:40 +0530

    'More.xaml' displays the favourite

--- WeatherLite/More.xaml.cs
+++ WeatherLite/More.xaml.cs
@@ -7,19 +7,64 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace WeatherLite
 {
     public partial class More : PhoneApplicationPage
     {
+        string addr = null;
         public More()
         {
             InitializeComponent();
         }
+        public void fgetWeather()
+        {
+           // var faddress = "http://api.openweathermap.org/data/2.5/weather?q="+city+","+country;
+            WebClient fclient = new WebClient(); //Web Client is required to fetch data from a web service
+            Uri furi = new Uri(addr); // Constructing a uri from the address as URI is required by DownloadAsync method of WebClient Object
 
+            fclient.DownloadStringCompleted += fclient_DownloadStringCompleted;
+            //client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadStringCallback2); // it is called when download from remote source is complete
+            fclient.DownloadStringAsync(furi);
+
+        }
+
+        private void fclient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            // Create the Json serializer and parse the response
+            var fobj = JsonConvert.DeserializeObject<Weath>(e.Result);
+            this.DataContext = fobj;
+            fvmintemp.Text = fobj.main.temp_min.ToString();
+            fvmaxtemp.Text = fobj.main.temp_max.ToString();
+            fvtemp.Text = fobj.main.temp.ToString();
+            fvwind.Text = fobj.wind.ToString();
+
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            favourite2.Visibility = Visibility.Collapsed;
+            frfile();
+            fgetWeather();
         }
+
+        private void frfile()
+        {
+            IsolatedStorageFile file = IsolatedStorageFile.GetUserStoreForApplication();
+
+            IsolatedStorageFileStream fs = file.OpenFile("state.txt", FileMode.Open, FileAccess.Read);
+
+            using (StreamReader sr = new StreamReader(fs))
+            {
+
+                String str = sr.ReadLine();
+                String[] temp = str.Split(',');
+                addr = "http://api.openweathermap.org/data/2.5/weather?q=" + temp[0] + "," + temp[1];
+                fvcity.Text = temp[0];
+                fvcountry.Text = temp[1];               
+            }
+        }
+
     }
 }
